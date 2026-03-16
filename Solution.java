3ea3a2be68863ecccebadfe87935154bb2082421
srijan-mahajan <163235748+srijan-mahajan@users.noreply.github.com>
2026-03-16 21:19:09 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,23 @@
+CONCATENATION OF ARRAY: 1929
+import java.util.Arrays;
+
+public class concatenation {
+    public static void main(String[] args) {
+        int []arr={1,2,1};
+
+       int []result=conArray(arr);
+        System.out.println(Arrays.toString(result));
+    }
+    static int[]conArray(int []arr)
+    {
+        int ans[] =new int[arr.length*2];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i]=arr[i];
+        }
+        for (int i = 0; i <arr.length ; i++) {
+            ans[i+arr.length]+=arr[i];
+        }
+        return ans;
+    }
+
+}
